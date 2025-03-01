f815e7f8b52ab288e34160808c3da768185eb755
Huy Vo <forwork@minhuy.dev>
2025-03-01 16:37:20 +0700

    serialization

--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer'
 import { IsString } from 'class-validator'
 
 export class LoginBodyDTO {
@@ -12,6 +13,19 @@ export class RegisterBodyDTO extends LoginBodyDTO {
   @IsString({ message: 'Name must be a string' })
   name: string
 
-  // @IsString()
+  @IsString()
   confirmPassword: string
+}
+
+export class RegisterResponseDTO {
+  id: number
+  email: string
+  name: string
+  @Exclude() password: string
+  createdAt: Date
+  updatedAt: Date
+
+  constructor(partial: Partial<RegisterResponseDTO>) {
+    Object.assign(this, partial)
+  }
 }
