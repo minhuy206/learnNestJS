2bdf19b0a2f4ec5cca1cb508809729155cb4562e
Huy Vo <forwork@minhuy.dev>
2025-03-02 11:08:18 +0700

    interceptor

--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,5 +1,6 @@
-import { Exclude } from 'class-transformer'
+import { Exclude, Type } from 'class-transformer'
 import { IsString } from 'class-validator'
+import { SuccessResponseDTO } from 'src/shared/shared.dto'
 
 export class LoginBodyDTO {
   @IsString()
@@ -17,7 +18,7 @@ export class RegisterBodyDTO extends LoginBodyDTO {
   confirmPassword: string
 }
 
-export class RegisterResponseDTO {
+export class RegisterData {
   id: number
   email: string
   name: string
@@ -25,7 +26,17 @@ export class RegisterResponseDTO {
   createdAt: Date
   updatedAt: Date
 
+  constructor(partial: Partial<RegisterData>) {
+    Object.assign(this, partial)
+  }
+}
+
+export class RegisterResponseDTO extends SuccessResponseDTO {
+  @Type(() => RegisterData)
+  data: RegisterData
+
   constructor(partial: Partial<RegisterResponseDTO>) {
+    super(partial)
     Object.assign(this, partial)
   }
 }
