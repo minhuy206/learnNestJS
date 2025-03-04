b074b1cd7d1e1ac04fcb8d72c0bdf00839815d86
Huy Vo <forwork@minhuy.dev>
2025-03-04 12:07:54 +0700

    custom IsMatch decorator

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,6 +1,12 @@
-import { LoginBodyDTO, LoginResponseDTO, RegisterBodyDTO, RegisterResponseDTO } from './auth.dto'
+import {
+  LoginBodyDTO,
+  LoginResponseDTO,
+  RefreshTokenResponseDTO,
+  RegisterBodyDTO,
+  RegisterResponseDTO,
+} from './auth.dto'
 import { AuthService } from './auth.service'
-import { Body, Controller, Post, SerializeOptions } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Post, SerializeOptions } from '@nestjs/common'
 
 @Controller('auth')
 export class AuthController {
@@ -9,8 +15,6 @@ export class AuthController {
   @SerializeOptions({ type: RegisterResponseDTO })
   @Post('register')
   async register(@Body() body: RegisterBodyDTO) {
-    console.log('controller')
-
     // return new RegisterResponseDTO(await this.authService.register(body)) // This is the original code
     return await this.authService.register(body)
   }
@@ -18,5 +22,11 @@ export class AuthController {
   @Post('login')
   async login(@Body() body: LoginBodyDTO) {
     return new LoginResponseDTO(await this.authService.login(body))
+  }
+
+  @Post('refresh-token')
+  @HttpCode(HttpStatus.OK)
+  async refreshToken(@Body() body: RefreshTokenResponseDTO) {
+    return new RefreshTokenResponseDTO(await this.authService.refreshToken(body.refreshToken))
   }
 }
